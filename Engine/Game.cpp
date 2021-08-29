7833633fd339e6366aa1cc810e3e88fc12988e07
CLRminded <the_rocket_scientist@icloud.com>
2021-08-29 01:17:27 -0700

    created a variable and using variables and operations to move radical around

--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -41,7 +41,30 @@ void Game::UpdateModel()
 {
 }
 
+/**
+* So the very first thing that we learn is that we have the power to control the pixels on the screen with PutPixel.
+* PutPixel is a method from the gfx class. We included the "Graphics.h" at the top that gives us the ability to use it.
+* The parameters are the x and y coordinates and the last 3 params are the rbg values that we get the color.
+* Easiest and most common way we will see PutPixels will be using variables x and y with operations to move. 
+* (This will be greatly improved in the future)
+*/
 void Game::ComposeFrame()
 {
-	gfx.PutPixel(300, 300, 255, 0, 0);
+	int x = 100;
+	int y = 500;
+
+	gfx.PutPixel(x - 5, y, 255, 255, 255);
+	gfx.PutPixel(x - 4, y, 255, 255, 255);
+	gfx.PutPixel(x - 3, y, 255, 255, 255);
+	gfx.PutPixel(x + 3, y, 255, 255, 255);
+	gfx.PutPixel(x + 4, y, 255, 255, 255);
+	gfx.PutPixel(x + 5, y, 255, 255, 255);
+	gfx.PutPixel(x, y - 5, 255, 255, 255);
+	gfx.PutPixel(x, y - 4, 255, 255, 255);
+	gfx.PutPixel(x, y - 3, 255, 255, 255);
+	gfx.PutPixel(x, y + 3, 255, 255, 255);
+	gfx.PutPixel(x, y + 4, 255, 255, 255);
+	gfx.PutPixel(x, y + 5, 255, 255, 255);
+
+
 }
